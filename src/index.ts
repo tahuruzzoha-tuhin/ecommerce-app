a7179c5fd64e9de5b8a6fa649e562d74883dba65
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2024-07-05 10:22:44 +0600

    [ADD] connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import express, { Request, Response } from 'express';
-const app = express()
-const port = 3000
+import express, {NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.MONGO_URI;
+
+async function main() {
+  try {
+  
+    await mongoose.connect(DB_URI as string);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT} with MongoDB`);
+    });
+    app.get('/',(req:Request, res:Response)=>{
+      res.send({"message":"Server running with mongodb atlas"})
+    })
+
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err);
+  }
+}
+
+main();
