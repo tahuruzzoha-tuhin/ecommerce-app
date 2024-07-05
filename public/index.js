3052d1d37c9ccba577312ee67694256e230078ff
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2024-07-05 10:24:19 +0600

    [FIX] env variables

--- public/index.js
+++ public/index.js
@@ -1,14 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.MONGO_URI;
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield mongoose_1.default.connect(DB_URI);
+            app.listen(PORT, () => {
+                console.log(`Server is running on http://localhost:${PORT} with MongoDB`);
+            });
+            app.get('/', (req, res) => {
+                res.send({ "message": "Server running with mongodb atlas" });
+            });
+        }
+        catch (err) {
+            console.error('Failed to connect to MongoDB:', err);
+        }
+    });
+}
+main();
